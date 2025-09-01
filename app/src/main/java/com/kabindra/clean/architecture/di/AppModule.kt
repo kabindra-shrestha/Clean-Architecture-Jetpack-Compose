60d28c9113909eb36c8b3cc822db52962c9cba2b
Kabindra Shrestha <kabindra@veelapp.com>
2025-09-01 15:49:26 +0545

    Removed deprecated viewmodel imports

--- app/src/main/java/com/kabindra/clean/architecture/di/AppModule.kt
+++ app/src/main/java/com/kabindra/clean/architecture/di/AppModule.kt
@@ -66,8 +66,8 @@ import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
-import org.koin.core.module.dsl.*
 import org.koin.dsl.module
 
 val provideAppModule = module {
